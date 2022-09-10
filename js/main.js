c5ea03a7b98d37821a3dbd33a08f4ceb5be7aba5
zomochii <zoheyy108@naver.com>
2022-09-10 20:52:36 +0900

    modify scroll (lodash cdn)

--- js/main.js
+++ js/main.js
@@ -14,3 +14,16 @@ searchInputEl.addEventListener('blur', function(){    //input요소에 focus가 
   searchEl.classList.remove('focused');  //focused라는 클래스 제거
   searchInputEl.setAttribute('placeholder', ''); //html의 속성을 'Attribute'라고 부름
 });
+
+
+const badgeEl = document.querySelector("header .badges");    //document는 html 자체
+
+window.addEventListener('scroll', _.throttle(function(){   //window : 우리가 보고있는 화면 창 , lodash cdn 연결 통해 _.throttle 사용가능
+  console.log(window.scrollY)
+  if(window.scrollY > 500){
+    badgeEl.style.display = 'none';   //배지 숨김
+  } else {
+    badgeEl.style.display = 'block';  //배지 보임
+  }
+}, 300));   //300ms = 0.3초   로 부하를 줘서 함수가 마구잡이로 실행되는 것 방지 (사이트를 가볍게 함)
+// _.throttle(함수, 시간)
